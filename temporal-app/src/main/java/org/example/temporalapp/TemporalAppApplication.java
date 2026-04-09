e22ebc75c13a535806b682d5a29cd1f21375cea6
Sasikiran-31 <kirankumar.2001@yahoo.in>
2026-04-09 12:51:36 -0400

    create-temporal-app

---
+++ temporal-app/src/main/java/org/example/temporalapp/TemporalAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.temporalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemporalAppApplication.class, args);
+    }
+
+}
---
+++ temporal-app/src/test/java/org/example/temporalapp/TemporalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.temporalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
